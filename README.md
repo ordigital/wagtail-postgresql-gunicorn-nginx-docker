56bed6ce5ca50b8cedf250f352db6fa539eba266
Ordigital <biuro@ordigital.pl>
2023-08-02 11:48:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+**Wagtail + PostgreSQL + Gunicorn + Nginx**
+
+1. Download repository: `git clone git@github.com:ordigital/wagtail-postgresql-gunicorn-nginx-docker.git`
+2. Create `.dev`, `.dev.db`, `.prod` and `.prod.db` config files in `env/` folder (you can rename and edit sample files)
+3. Make `wag` file executable: `chmod +x ./wag`
+4. Use: `./wag` to stop, build and start developement server (Django internal server on port 8000) and `./wag prod` to stop, build and start production server (with gunicorn and nginx proxy on port 8000)
---
+++ app/app/settings/production.py
@@ -0,0 +1,24 @@
+import os
+from .base import *
+
+DEBUG = os.environ.get("DEBUG")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
+
+# Database PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE"),
+        'NAME': os.environ.get("SQL_DATABASE"),
+        'USER': os.environ.get("SQL_USER"),
+        'PASSWORD': os.environ.get("SQL_PASSWORD"),
+        'HOST': os.environ.get("SQL_HOST"),
+        'PORT': os.environ.get("SQL_PORT"),
+    }
+}
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
